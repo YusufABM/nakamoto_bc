0c7a09032f79e9c29e6ca7e842bbcd568b4f447f
Yusuf <201703768@post.au.dk>
2024-09-17 18:15:54 +0200

    Add account and ledger functionality using code from the exercise

---
+++ account/account.go
@@ -0,0 +1,38 @@
+package account
+
+import (
+	"sync"
+)
+
+// From assignment description
+type Transaction struct {
+	ID     string
+	From   string
+	To     string
+	Amount int
+}
+
+// Ledger is a map of account names to balances
+// sync.Mutex = It is safe for concurrent use
+type Ledger struct {
+	Accounts map[string]int
+	lock     sync.Mutex
+}
+
+// Creates a new instance of Ledger with an empty map
+// Returns a pointer to the new Ledger
+func MakeLedger() *Ledger {
+	Ledger := new(Ledger)
+	Ledger.Accounts = make(map[string]int)
+	return Ledger
+}
+
+// Updates the ledger with the transaction
+// Defer unlocks the mutex at the end of the function
+func (l *Ledger) Transaction(t *Transaction) {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+
+	l.Accounts[t.From] -= t.Amount
+	l.Accounts[t.To] += t.Amount
+}
